c2520c62528e073f6059bf780d63f0ea4740fa1c
YKCzoli <yuriy.czoli@gmail.com>
2016-10-24 14:07:33 -0400

    add a third layer

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,21 @@
 
   var mymap = new L.Map('mapid', {
-  center: new L.LatLng(45.50144, -122.67599),
-  zoom: 4,
+  center: new L.LatLng(41.028814, 28.948970),
+  zoom: 0,
   minZoom: 0,
   maxZoom: 18,
   layers: [
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-      maxZoom: 9,
+      maxZoom: 4,
       minZoom: 0,
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      id: 'mapbox.streets'
+    }),
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+      maxZoom: 9,
+      minZoom: 5,
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
   			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
   			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
